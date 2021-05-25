e710faf8fd9c9f019d365d717697a0034ab90135
MiguelMarcelino <mac.marcelino@outlook.pt>
2021-05-25 15:45:34 +0100

    Advanced on project

--- JuliaProject/Test.java
+++ JuliaProject/Test.java
@@ -6,6 +6,8 @@ public class Test {
         Method[] methods = test.getClass().getMethods();
         methods[0].getName();
         methods[0].getParameterTypes();
+        Math.class.getMethods();
+        // Class.forName("java.lang.Math");
     }
 
     public void test(int ola) {
--- JuliaProject/javaImporter.jl
+++ JuliaProject/javaImporter.jl
@@ -4,33 +4,50 @@ using JavaCall
 
 JavaCall.init(["-Xmx128M"])
 
-# Stores class alias as key and JCallInfo as class information
-importedClasses = Dict{String, JCallInfo}
-
-typeMapper = Dict{String, }
-
 struct JCallInfo
     ref: JavaObject
     methods::Dict
 end
 
+# Stores class alias as key and JCallInfo as class information
+importedClasses = Dict{String, JCallInfo}
+
+
+# typeMapper = Dict{String, }
+
+function Base.show(io::IO, ::MIME"text/plain", v::Vector{JMethod}) where {T}
+    println(io, "Vector with type $T and elements")
+    for i in eachindex(v)
+        println(io, "  ", v.v[i])
+    end
+end
+
+# Base.show(io::IO, obj::JavaObject) = print(io, jcall(obj, "toString", JString, ()))
+# Base.show(io::IO, obj::Vector{JMethod}) = print(io, jcall(obj, "toString", JString, ()))
+# Base.show(io::IO, jv::JCallInfo) = show(io, getfield(jv, :ref))
+# Base.getproperty(jv::JCallInfo, sym::Symbol) =getfield(jv, :methods)[sym](getfield(jv, :ref))
+
 # jlM = @jimport java.lang.Math
 function javaImport(fullPath::String)
-    jlM = @jimport fullPath
-    class = jcall(jlM, getClass, JObject, ())
-    methods = jcall(class, getMethods, JObject, ())
     # 1) Put methods in dictionary (somehow)
     # 2) Create function called fullPath that calls necessary 
     #    methods from dictionary
+
+    class = classforname(fullPath)
+    # j_u_arrays = @jimport java.util.Arrays
+    methods() = jcall(class, "getMethods", Vector{JMethod}, ())
+
+    methodsDict = Dict()
+
+    classInfo = JCallInfo(class)
 
     # TODO
     Meta.parse(fullPath * "(methodName, args...) = {
         
     }")
     
-function typeMapper(javaType::String) {
-    
-}
+# function typeMapper(javaType::String) {
+# }
 
 
-jcall(jlM, "sin", jdouble, (jdouble,), pi/2)
+# jcall(jlM, "sin", jdouble, (jdouble,), pi/2)
