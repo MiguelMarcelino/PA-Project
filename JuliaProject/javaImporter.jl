7c20b836a5894c38d6c625ba86b0dada63265043
MiguelMarcelino <mac.marcelino@outlook.pt>
2021-05-26 12:05:56 +0100

    Testing with importedClasses dictionary

--- JuliaProject/javaImporter.jl
+++ JuliaProject/javaImporter.jl
@@ -10,28 +10,12 @@ struct JCallInfo
 end
 
 # Stores class alias as key and JCallInfo as class information
-importedClasses = Dict{String, JCallInfo}
+importedClasses = Dict{String, JCallInfo}()
 
-# typeMapper = Dict{String, }
-
-# function Base.show(io::IO, ::MIME"text/plain", v::Vector{JMethod}) where {T}
-#     println(io, "Vector with type $T and elements")
-#     for i in eachindex(v)
-#         println(io, "  ", v.v[i])
-#     end
-# end
-
-# Base.show(io::IO, obj::JavaObject) = print(io, jcall(obj, "toString", JString, ()))
-# Base.show(io::IO, obj::Vector{JMethod}) = print(io, jcall(obj, "toString", JString, ()))
-# Base.show(io::IO, jv::JCallInfo) = show(io, getfield(jv, :ref))
-# Base.getproperty(jv::JCallInfo, sym::Symbol) =getfield(jv, :methods)[sym](getfield(jv, :ref))
-
-# jlM = @jimport java.lang.Math
-methodsDict = Dict()
-
-function javaImport(fullPath::String)
+function javaImport(alias::String, fullPath::String)
     class = classforname(fullPath)
     methods() = jcall(class, "getMethods", Vector{JMethod}, ())
+    methodsDict = Dict()
 
     for method in (methods())
         methodName = jcall(method, "getName", JString,())
@@ -43,10 +27,13 @@ function javaImport(fullPath::String)
         finalParamNames = tuple(parameterNames...)
         get!(methodsDict,Pair(methodName,finalParamNames),method)
     end
+
+    get!(importedClasses, alias, JCallInfo(class, methodsDict))
+    # Meta.parse(alias * " = 
+    #     importedClasses[alias]")
 end
 
-math = JCallInfo(class, methodsDict)
-math.abs(4)
+
 #get(methodsDict,Pair("abs",Vector{classforname("java.lang.Float")}))
 
 # jcall(first, "getName", JString,())
